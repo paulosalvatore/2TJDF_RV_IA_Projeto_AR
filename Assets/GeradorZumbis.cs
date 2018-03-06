26efa4248baa8ded24a637c6b74d081387cc9171
Paulo Salvatore <salvatoregames@gmail.com>
2018-03-06 11:55:24 -0300

    Initial commit

---
+++ Assets/GeradorZumbis.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorZumbis : MonoBehaviour
+{
+	public GameObject zumbiPrefab;
+
+	[Range(0.1f, 10f)]
+	public float delayZumbi = 2f;
+
+	private void Start()
+	{
+	}
+
+	private void Update()
+	{
+	}
+
+	public void IniciarGeradorZumbis(GameObject spawn)
+	{
+		StartCoroutine(GerarZumbis(spawn));
+	}
+
+	public void EncerrarGeradorZumbis()
+	{
+		StopAllCoroutines();
+	}
+
+	private IEnumerator GerarZumbis(GameObject spawn)
+	{
+		while (true)
+		{
+			GameObject instancia = Instantiate(zumbiPrefab);
+
+			instancia.transform.parent = spawn.transform.parent;
+
+			Vector2 posicaoAleatoria = Random.insideUnitCircle;
+
+			instancia.transform.position = new Vector3(
+				posicaoAleatoria.x,
+				0,
+				posicaoAleatoria.y
+			);
+
+			yield return new WaitForSeconds(delayZumbi);
+		}
+	}
+}
